cc261422173c618d079973ed665882c99a900bd7
Saïsana <khamvongsa.v@hotmail.fr>
2025-04-09 17:59:55 +0200

    Récupération et affichage des catégories

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -5,8 +5,8 @@
  *********************************************************************************/
 
 // Récupération des projets (works) depuis l'api
-const reponse = await fetch('http://localhost:5678/api/works');
-const works = await reponse.json();
+const reponseWorks = await fetch('http://localhost:5678/api/works');
+const works = await reponseWorks.json();
 
 function generateWorks(works){
     for (let i = 0; i < works.length; i++) {
@@ -22,7 +22,7 @@ function generateWorks(works){
         const titleElement = document.createElement("figcaption");
         titleElement.innerText = figure.title;
         
-        // On rattache la balise figure a la section Gallery
+        // On rattache la balise figure à la section Gallery
         divGallery.appendChild(workElement);
         workElement.appendChild(imageElement);
         workElement.appendChild(titleElement);
@@ -30,3 +30,31 @@ function generateWorks(works){
 }
 
 generateWorks(works);
+
+// Affichage des catégories
+const reponseCategories = await fetch('http://localhost:5678/api/categories');
+const categories = await reponseCategories.json();
+
+function generateCategories(categories){
+    // Récupération de l'élément du DOM qui accueillera les Catégories
+    const divCategories = document.querySelector(".categories");
+    const firstbuttonNameElement = document.createElement("button");
+    //Création de la Catégorie "Tous"
+        firstbuttonNameElement.innerText = "Tous";
+        divCategories.appendChild(firstbuttonNameElement);
+
+    for (let i = 0; i < categories.length; i++) {
+        
+        const button = categories[i];
+        
+        // Création d’une balise dédiée à une catégorie
+        const buttonNameElement = document.createElement("button");
+        buttonNameElement.innerText = button.name;
+        
+        // On rattache la balise figure à la section Catégories
+        divCategories.appendChild(buttonNameElement);
+        //buttonNameElement.appendChild(categoryElement);
+     }
+}
+
+generateCategories(categories);
